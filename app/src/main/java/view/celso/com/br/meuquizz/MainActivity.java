408fd30e23708af9e251fca896239425bb1ba026
celso andre <celsolog2012@gmail.com>
2017-01-09 15:41:24 -0200

    commit inicial

---
+++ app/src/main/java/view/celso/com/br/meuquizz/MainActivity.java
@@ -0,0 +1,13 @@
+package view.celso.com.br.meuquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
